82f2750cb23faa2fe169ad25c9e819ddb507e36b
loverXDR <106595748+loverXDR@users.noreply.github.com>
2023-06-06 18:44:41 +0300

    Update lab3.js

--- javascript/lab3.js
+++ javascript/lab3.js
@@ -1,9 +1,19 @@
 "use strict";
-
+/**
+ * возвращает дробную часть числа num
+ * @param {number} num - возводимое число в степень
+ * @return {number} - возвращает дробную часть
+ */
 export function getDecimal(num) {
   let w = Math.abs(num - Math.floor(num));
   return parseFloat (w.toFixed(2));
 }
+
+/**
+ * возвращает строку str с заглавным первым символом
+ * @param {string} str - входная строка
+ * @return {string} - Строка с первой буквой, написанной с заглавной буквы
+ */
 
 export function ucFirst(str) {
   if (!str) {
@@ -13,11 +23,25 @@ export function ucFirst(str) {
   }
 }
 
+/** 
+* возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false
+* @param {string} str - Строка для проверки.
+* @returns {boolean} - Возвращает true, если строка содержит слова "viagra" или "xxx", иначе - false.
+*/
+
 export function checkSpam(str) {
   let lowerStr = str.toLowerCase();
   
   return lowerStr.includes('viagra') || lowerStr.includes('xxx');
 }
+
+/**
+ * проверяет длину строки str, и если она превосходит maxlength – заменяет конец str на символ многоточие "…"
+  * @param {string} str - строка, которую нужно обрезать
+  * @param {number} maxlength - максимальная длина строки
+  * @return {string} - обрезанная строка или исходная строка, если ее длина не превышает максимальную
+ */
+
 export function truncate(str, maxlength) {
   if (str.length > maxlength) {
     return str.slice(0, maxlength - 1) + '…';
@@ -25,6 +49,13 @@ export function truncate(str, maxlength) {
     return str;
   }
 }
+
+/**
+ * преобразует строку вида 'var-test-text' в 'varTestText'
+ * @param {string} str - Строка для преобразования
+ * @returns {string} - соединяет [var test text] в 'varTestText'
+ */
+
 export function camelize(str) {
   let words = str.split('-');
   
@@ -34,7 +65,22 @@ export function camelize(str) {
   
   return words.join('');
 }
+
+
+
+/**
+ * возвращает n-е число Фибоначчи
+ * @param {number} n - число Фибоначчи, до которого необходимо всё вычислить
+ * @return {array} - Массив, содержащий последовательность Фибоначчи с точностью до n
+ */
+
 import { fib } from './fibfunk.js';
+
+/**
+ * для натурального n возвращает массив, заполненный числами Фибоначчи до n-го (не включая его)
+ * @param {number} n -  число, до которого вычисляется массив из чисел Фибоначчи
+ * @returns {array} - массив из чисел Фибоначчи с точностью до заданного числа
+ */
 
 export function fibs(n) {
   const arr = [];

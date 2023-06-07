5eca79bf31e2de8b2eb90a521d3a1ed1f11b1b22
loverXDR <loverxdr@yandex.ru>
2023-06-07 13:08:09 +0300

    Update lab1_1.js

--- javascript/lab1_1.js
+++ javascript/lab1_1.js
@@ -60,7 +60,7 @@ function checkNumber() {
   let num;
   do {
     num = prompt("Введите число больше 100:");
-  } while (num <= 100 && num !== null || isNaN(num));
+  } while (num !== null && (isNaN(num) || +num <= 100));
 }
 
 function checkPrime() {
